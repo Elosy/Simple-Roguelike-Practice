82cac7c54c73de875ff78636e23f121188ee9122
elosy <gaudic99@gmail.com>
2026-07-05 11:53:52 +0200

    scene loading, main menu

--- game_manager.gd
+++ game_manager.gd
@@ -4,3 +4,25 @@ const GAME_SCENE: PackedScene = preload("uid://5diwa0vg5tfm")
 const MAIN_MENU_SCENE: PackedScene = preload("uid://bxau651bwsp0d")
 
 var current_child: Node
+
+
+func _ready():
+	load_main_menu()
+
+
+func switch_to_scene(scene: PackedScene) -> Node:
+	if current_child != null:
+		current_child.queue_free()
+	current_child = scene.instantiate()
+	add_child(current_child)
+	return current_child
+
+
+func load_main_menu() -> void:
+	var main_menu: MainMenu = switch_to_scene(MAIN_MENU_SCENE)
+	main_menu.game_requested.connect(_on_game_requested)
+
+
+func _on_game_requested(try_load: bool) -> void:
+	var game: GameRoot = switch_to_scene(GAME_SCENE)
+	game.main_menu_requested.connect(load_main_menu)
